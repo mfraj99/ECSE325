1b26b2ef3bb8b72fdce957da9271ac5196ccc97d
Shi Tong Li <46496145+link825@users.noreply.github.com>
2020-04-07 20:25:10 -0400

    workbench and vhdl not done

--- lab3/g42_FIR.vhd
+++ lab3/g42_FIR.vhd
@@ -12,13 +12,57 @@ port(	x		:in std_logic_vector(15 downto 0); --input singla
 end g42_FIR;
 
 architecture a0 of g42_FIR is
+type arr is array(24 downto 0) of signed(15 downto 0);
 signal temp: signed(31 downto 0);
+signal coeff: arr;
+signal input: arr;
+
+
 begin
+--values of the coeff 
+coeff(0) <= "";
+coeff(1) <= "";
+coeff(2) <= "";
+coeff(3) <= "";
+coeff(4) <= "";
+coeff(5) <= "";
+coeff(6) <= "";
+coeff(7) <= "";
+coeff(8) <= "";
+coeff(9) <= "";
+coeff(10) <= "";
+coeff(11) <= "";
+coeff(12) <= "";
+coeff(13) <= "";
+coeff(14) <= "";
+coeff(15) <= "";
+coeff(16) <= "";
+coeff(17) <= "";
+coeff(18) <= "";
+coeff(19) <= "";
+coeff(20) <= "";
+coeff(21) <= "";
+coeff(22) <= "";
+coeff(23) <= "";
+coeff(24) <= "";
+
+
 process(clk)
+begin
 	if rst = '1' then
-		temp <="00000000000000000000000000000000";
+		input <= (others=>(others=>'0'));
 	elsif rising_edge(clk) then
+		temp <="00000000000000000000000000000000";
 		for i in 0 to 24 loop
-			temp = temp + signed(coeff) * signed(x
+			input(25-i) <= input(25-(i+1));
+		end loop;
+		input(0) <= signed(x);
+		
+		for i in 1 to 24 loop
+			temp <= temp + coeff(25-i)*input(25-i);
+		end loop;
+	end if;
+end process;
+y <= std_logic_vector(temp(31 downto 15));
 		
 end a0;
---
+++ lab3/g42_lab3_tb.vhd
@@ -0,0 +1,81 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+entity g42_FIR_tb is
+end g42_FIR_tb;
+
+architecture testbench of g42_FIR_tb is
+
+
+
+	component g42_FIR is 
+		port ( 	
+			x		: in std_logic_vector(15 downto 0); --input signal
+			clk		: in std_logic;  -- clock
+			rst		: in std_logic;  -- asynchronous active-high reset
+			y 		: out std_logic_vector(16 downto 0) -- output signal
+		);
+	end component g42_FIR;
+
+	file file_IN 		: text;
+	file file_RESULTS 	: text;
+	
+	constant clk_PERIOD : time := 60 ns;
+	
+	signal x_in		: std_logic_vector(15 downto 0);
+	signal clk_in		: std_logic;
+	signal rst_in		: std_logic;
+	signal y_out		: std_logic_vector(16 downto 0);
+
+begin
+		g42_FIR_test : g42_FIR
+		port map (
+			x 	=> x_in,
+			clk 	=> clk_in,
+			rst 	=> rst_in,
+			y 		=> y_out
+		);
+	
+--CLOCK GENERATION
+	clk_gen : process 
+		 begin 
+			  clk_in <= '1';
+			  wait for clk_PERIOD / 2; 
+			  clk_in <= '0';
+			  wait for clk_PERIOD / 2; 
+	end process clk_gen;
+				
+
+	feeding_instr : process is 
+		variable v_lline1 : line;
+		variable v_lline2 : line; 
+		variable v_Oline  : line; 
+		variable v_x_in   : std_logic_vector(15 downto 0);
+	 
+	begin
+
+		rst_in <= '1';
+		wait until rising_edge(clk_in);
+		wait until rising_edge(clk_in);
+		rst_in <= '0';
+
+		file_open(file_IN, "C:\", read_mode);
+		file_open(file_RESULTS, "C:\", write_mode);
+		
+		while not endfile (file_IN) loop
+			readline(file_IN, v_lline1);	
+			read(v_lline1, v_x_in);
+			x_in <= v_x_in;
+			write(v_Oline, y_out);
+			writeline(file_RESULTS, v_Oline);
+			wait until rising_edge(clk_in);
+		 end loop;
+		 wait until rising_edge(clk_in);
+		 wait until rising_edge(clk_in);
+	 	 wait;
+	end process feeding_instr;
+end testbench;
+
